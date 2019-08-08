d7643d581f5f817709f4254a3905fd1df90d2a48
lxxue <lxxue@buaa.edu.cn>
2019-08-08 09:39:01 +0000

    add test specification

--- test/run.sh
+++ test/run.sh
@@ -2,8 +2,8 @@
     python -m torch.distributed.launch --nproc_per_node=4 \
     --master_addr="127.0.0.1" --master_port=8765 \
     test.py \
-    seg_r50_nofcn_2x.py \
-    epoch_24.pth \
-    --out 24_metric.pkl \
+    $1 \
+    $2 \
+    --out $3 \
     --launcher pytorch
 
